522d704babbb744e801947ccb5bb190f5e8d3fc2
Khanh Ha <hakhanh.1106@gmail.com>
2017-10-16 16:26:20 +0700

    Add project files.

---
+++ BigSchool_Khanh2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigSchool_Khanh2.MvcApplication" Language="C#" %>
---
+++ BigSchool_Khanh2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_Khanh2.Startup))]
+namespace BigSchool_Khanh2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
